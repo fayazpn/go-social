30a5bf6a1cc28354262f30e397e99c3e2c837b64
Fayaz Basheer <pnfayaz@gmail.com>
2024-11-10 16:57:38 +0400

    create db connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"log"
+	"social/internal/db"
 	env "social/internal/env"
 	"social/internal/store"
 )
 
 func main() {
-	cfg := config{addr: env.GetString("ADDR", ":8081")}
+	cfg := config{
+		addr: env.GetString("ADDR", ":8081"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
+	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewStorage(db)
 	app := &application{config: cfg, store: store}
 	mux := app.mount()
 
