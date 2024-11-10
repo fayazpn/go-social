48f73d0c499ffe775ebcb81f02f09ca552f79d65
Fayaz Basheer <pnfayaz@gmail.com>
2024-11-10 16:18:28 +0400

    implement user and post store methods

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,6 +3,8 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pq"
 )
 
 type Post struct {
@@ -19,6 +21,14 @@ type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostStore) Create(ctx context.Context) error {
+func (s *PostStore) Create(ctx context.Context, post *Post) error {
+	query := `INSERT INTO posts (content, title, user_id, tags) VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at`
 
+	err := s.db.QueryRowContext(ctx, query, post.Content, post.Title, post.UserId, pq.Array(post.Tags)).Scan(&post.UserId, &post.CreatedAt, &post.UpdatedAt)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
