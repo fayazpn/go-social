17de8b12d4945d6543b0e14b48acc46f0a282ba5
Fayaz Basheer <pnfayaz@gmail.com>
2024-11-19 18:52:45 +0400

    json marshelling

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -3,6 +3,12 @@ package main
 import "net/http"
 
 func (app *application) handleCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ok"))
+	data := map[string]string{
+		"status":  "ok",
+		"env":     app.config.env,
+		"version": version,
+	}
+	if err := writeJSON(w, http.StatusOK, data); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, "Server error")
+	}
 }
